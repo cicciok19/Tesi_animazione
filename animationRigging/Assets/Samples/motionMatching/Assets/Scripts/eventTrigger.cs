5b2aadc039b017b12a6a0947316ea6ad0d710d56
Francesco <Cali>
2021-03-16 11:03:22 +0100

    Traslazione sit ok

--- animationRigging/Assets/Samples/motionMatching/Assets/Scripts/eventTrigger.cs
+++ animationRigging/Assets/Samples/motionMatching/Assets/Scripts/eventTrigger.cs
@@ -11,16 +11,12 @@ public class eventTrigger : MonoBehaviour
 
     [SerializeField]
     private MxMEventDefinition m_attackDefinition;
-
     [SerializeField]
     private MxMEventDefinition m_greetingsDefinition;
-
     [SerializeField]
     private MxMEventDefinition m_sitDefinition;
-
     [SerializeField]
     private MxMEventDefinition m_standUpDefinition;
-
     [SerializeField]
     private Transform sitPoint;
 
@@ -35,6 +31,7 @@ public class eventTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.U))
         {
             m_animator.BeginEvent(m_attackDefinition);
@@ -48,7 +45,7 @@ public class eventTrigger : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.K) && !sit)
         {
             m_sitDefinition.ClearContacts();
-            m_sitDefinition.AddEventContact(sitPoint);
+            m_sitDefinition.AddEventContact(sitPoint.position, this.transform.rotation.y);
             m_animator.BeginEvent(m_sitDefinition);
             sit = true;
             m_animator.RemoveRequiredTag("Locomotion");
@@ -60,11 +57,6 @@ public class eventTrigger : MonoBehaviour
             m_animator.BeginEvent(m_standUpDefinition);
             sit = false;
             m_animator.SetRequiredTag("Locomotion");
-        }
-
-        if (m_animator.IsEventComplete)
-        {
-            m_animator.ForceExitEvent();
         }
     }
 }
