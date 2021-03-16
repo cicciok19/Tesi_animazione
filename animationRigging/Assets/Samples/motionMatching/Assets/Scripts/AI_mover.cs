743e670715e09e4cf74547a039b4923be1ff99e2
Francesco <Cali>
2021-03-16 17:19:00 +0100

    animData con mocap

--- animationRigging/Assets/Samples/motionMatching/Assets/Scripts/AI_mover.cs
+++ animationRigging/Assets/Samples/motionMatching/Assets/Scripts/AI_mover.cs
@@ -9,17 +9,23 @@ public class AI_mover : MonoBehaviour
     [SerializeField] private Transform destinationTransform_2;
     [SerializeField] private Transform destinationTransform_3;
     private NavMeshAgent navAgent;
+    private NavMeshPath path;
 
     // Start is called before the first frame update
     void Start()
     {
         navAgent = GetComponent<NavMeshAgent>();
+        navAgent.autoBraking = true;nav
         navAgent.SetDestination(destinationTransform_1.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(navAgent.pathStatus);
+        if(navAgent.pathStatus == NavMeshPathStatus.PathComplete)
+        {
+
+        }
     }
 }
